4812827c2044e7bc782a253e2396f2291280cf7a
Harshit <harshitjain8085083404@gmail.com>
2021-12-02 18:07:25 +0530

    Login Added

--- app/src/main/java/com/example/fakenewsdetectionandroidapp/Login.kt
+++ app/src/main/java/com/example/fakenewsdetectionandroidapp/Login.kt
@@ -1,18 +1,18 @@
-package com.example.fakenewsdetectionandroidapp.ui.login
+package com.example.fakenewsdetectionandroidapp
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.fakenewsdetectionandroidapp.R
+//import com.example.fakenewsdetectionandroidapp.R
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        val button = findViewById<Button>(R.id.button)
+        val button = findViewById<Button>(R.id.button2)
         button.setOnClickListener{
-           var intent=Intent(this@MainActivity,MainActivity::class.java)
+           val intent=Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
     }
